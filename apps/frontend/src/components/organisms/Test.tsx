e034c7a51eca293e834451b5113852dccf320ad6
Mickael <mickael.mesnage@lonestone.io>
2023-09-21 14:31:55 +0200

    test client component for seo

--- apps/frontend/src/components/organisms/Test.tsx
+++ apps/frontend/src/components/organisms/Test.tsx
@@ -5,5 +5,12 @@ import { useTranslations } from "next-intl";
 export const Test = () => {
   const t = useTranslations();
 
-  return <div>{t("test")}</div>;
+  return (
+    <>
+      <h1>{t("test")}</h1>
+      <h2>{t("test2")}</h2>
+      <p>{t("test3")}</p>
+      <p>{t("test4")}</p>
+    </>
+  );
 };
