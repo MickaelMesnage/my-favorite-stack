3200268f7dd4738d780c18d311c2695e3fc7098d
Mickael <mickael.mesnage@lonestone.io>
2023-09-24 14:26:51 +0200

    :construction: cors handler

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,14 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get<ConfigService>(ConfigService);
-  app.enableCors({
-    origin: [configService.get<string>('CLIENT_ORIGIN')],
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
+  // TODO enable cors for only client origin (but no trace in free render)
+  // const configService = app.get<ConfigService>(ConfigService);
+  // app.enableCors({
+  //   origin: [configService.get<string>('CLIENT_ORIGIN')],
+  // });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
