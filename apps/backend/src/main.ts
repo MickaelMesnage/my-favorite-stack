39b8c7b2643c5f6aa5d4e5ec37d612cc85d08187
Mickael <mickael.mesnage@lonestone.io>
2023-09-24 11:31:06 +0200

    :rocket: handle cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
+  app.enableCors({
+    origin: [configService.get<string>('CLIENT_ORIGIN')],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
